ca5eb4607b2a9a71a7c9a3f007a2e07502dc1fe2
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-06 05:45:35 +0100

    Error handling middleware added

--- app.js
+++ app.js
@@ -36,10 +36,28 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+// router to handle all unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err); // whenever an argument is passed into next(); function, express will know there was an error
+  // all other middlewares then will get skipped and we will receive the error
+});
+
+// when specifiyng 4 parameters, express knows this is a
+// error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
