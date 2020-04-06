ea48ec0a37e40d342a0e924bddf838c335c05bad
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-06 05:01:16 +0100

    Unhandled route router added

--- app.js
+++ app.js
@@ -36,5 +36,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
 // 4. START SERVER
 module.exports = app;
