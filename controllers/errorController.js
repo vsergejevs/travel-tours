fe78996bccccb5ffbeb9510fb5a33524baff82dc
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-08 06:06:55 +0100

    added a function to handle duplicate field in DB

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,6 +7,13 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/"(.*?)"/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -46,6 +53,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
