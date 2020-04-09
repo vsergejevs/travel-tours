b52731f887e69b106b1f9f8c6ffc17fe05847c94
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-09 05:11:09 +0100

    Code added to deal with unhandled rejections

--- server.js
+++ server.js
@@ -23,6 +23,15 @@ mongoose
 // console.log(process.env);
 
 const port = 3000 || process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Subscribing to unhandledRejection object
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message); // printing what's the error
+  console.log('UNHANDLED REJECTION Shutting down'); // friendly message
+  server.close(() => {
+    process.exit(1); //shutting down app
+  });
 });
