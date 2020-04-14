89ab007bc1d23d7726cc666647630e222eaa1e38
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-14 04:57:12 +0100

    bcrypt hash function added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -31,5 +32,18 @@ const userSchema = new mongoose.Schema({
     }
   }
 });
+
+userSchema.pre('save', async function(next) {
+  // only run this function if password was modified
+  if (!this.isModified('password')) return next();
+
+  // password is being encrypted with a bcrypt hash function, with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
