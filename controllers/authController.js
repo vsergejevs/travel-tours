ccebd3b98caf2210419345512a4477f5518332b0
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-16 05:01:32 +0100

    started immplementing route access protection middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,3 +53,25 @@ exports.login = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in!', 401));
+  }
+  // 2) Verify token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after JWT token was issued
+  next();
+});
