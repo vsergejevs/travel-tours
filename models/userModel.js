504d63bdf3beeb4c99d693be0193e30adca761fc
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-18 06:28:59 +0100

    added changedPasswordAfter method to check if user has changed password after token was issued

--- models/userModel.js
+++ models/userModel.js
@@ -31,7 +31,8 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Passwords do not match!'
     }
-  }
+  },
+  passwordChangedAt: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -51,6 +52,21 @@ userSchema.methods.correctPassword = async function(
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword); //compares if both passwords are same, and returns true if are
+};
+
+//instance method
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
