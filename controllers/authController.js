a0fa7258a6bc2dbfaeee73d6a3dc0d7a6fb86d57
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-19 07:08:45 +0100

    added restrictTo middleware to restrict user actions

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role,
     passwordChangedAt: req.body.passwordChangedAt
   });
 
@@ -91,3 +92,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide']
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    next();
+  };
+};
