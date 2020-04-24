670929fbfe6fe368c87cea531847e87a4876d606
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-24 05:16:31 +0100

    api rate limiting added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,12 +9,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1. MIDDLEWARES
+// 1. GLOAL MIDDLEWARES
 // The process.env property returns an object containing the user environment.
 //console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
 
 // Method - a built-in middleware function in Express. It parses incoming requests with JSON payloads
 app.use(express.json());
@@ -30,6 +39,7 @@ app.use((req, res, next) => {
 // Middleware to log request time in controllers
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
   next();
 });
 
