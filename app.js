e8047b22008f275ae833c239260066c2e64724e6
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-24 05:50:05 +0100

    data sanitization against NoSQL and XSS attacks added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body. Method - a built-in middleware function in Express. It parses incoming requests with JSON payloads
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files. Method - a built-in middleware function in Express. It serves static files
 app.use(express.static(`${__dirname}/public`));
