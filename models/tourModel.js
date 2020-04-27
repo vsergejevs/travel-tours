126639e2e2cc7f74e7e95ba98889c393a5f48a04
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-27 05:30:33 +0100

    GeoJSON support and location data added

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator'); // String validation library https://github.com/validatorjs/validator.js
+const User = require('./userModel');
 
 //dataschema https://mongoosejs.com/docs/guide.html#definition
 const tourSchema = new mongoose.Schema(
@@ -79,13 +80,39 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
   }
 );
+// GeoJson support and location data added to tourModel
 
 // Virtual properties wont be saved into database
 // Using a regular function instead of arrow functions as arrows dont get its own this keyword
@@ -97,6 +124,14 @@ tourSchema.virtual('durationWeeks').get(function() {
 // Document middleware: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Retrieve user documents corresponding to id's
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
   next();
 });
 
