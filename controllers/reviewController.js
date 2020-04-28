1074dac14ec0059a8e5711737c12e4dacca1c56a
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-28 06:27:21 +0100

    created review controller file. added getReview and createReview

---
+++ controllers/reviewController.js
@@ -0,0 +1,24 @@
+const Review = require('./../models/reviewModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review
+    }
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview
+    }
+  });
+});
