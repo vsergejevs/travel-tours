7f010563220e3005a0248764115e915409eefeb7
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-29 04:53:06 +0100

    edited function name

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,13 +1,14 @@
 const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.id);
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
 
   res.status(200).json({
     status: 'success',
+    results: reviews.length,
     data: {
-      review
+      reviews
     }
   });
 });
