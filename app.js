69b0c5604f81126d9467a20393e765c9cf10cabf
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-29 04:55:09 +0100

    added review route

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -73,6 +74,7 @@ app.use((req, res, next) => {
 //the below now acts as middleware
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // router to handle all unhandled routes
 app.all('*', (req, res, next) => {
