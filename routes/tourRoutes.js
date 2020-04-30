b970284831b8a90cd632b5b3c95a13891f04b1cc
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-04-30 05:31:08 +0100

    added review rerouting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const authController = require('./../controllers/authController');
 
 const tourController = require('./../controllers/tourController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 // router.param('id', tourController.checkId);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -27,14 +29,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
