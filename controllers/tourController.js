07fb8b297d38d4fa53a6ddc3f1e080634d00fffa
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-05-02 04:30:10 +0100

    replaced deleteTour with the new handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@ const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -76,18 +77,8 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-
-  if (!tour) {
-    return next(new AppError('No tour found with that id', 404));
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-});
+// Using the new factory function here
+exports.deleteTour = factory.deleteOne(Tour);
 
 // Data aggregation pipeline stages - MongoDB
 // https://docs.mongodb.com/manual/reference/operator/aggregation-pipeline/
