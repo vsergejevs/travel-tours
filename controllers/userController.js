5585d02a9f3cf3fcfbb1db5c34e16a0f6079d3a0
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-05-02 04:49:14 +0100

    factory handler delete added

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -85,9 +86,4 @@ exports.updateUser = (req, res) => {
   });
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
