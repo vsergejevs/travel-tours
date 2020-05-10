50913e0fbea1d29960374c949cc1abbf5fae3a33
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-05-10 07:36:09 +0100

    replaced files that had bugs

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator'); // String validation library https://github.com/validatorjs/validator.js
 // const User = require('./userModel');
+// const validator = require('validator');
 
-//dataschema https://mongoosejs.com/docs/guide.html#definition
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'A tour must have a price'],
+      required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
-      maxlength: [40, 'A tour name must have maximum of 40 characters'], // Backend validators
-      minlength: [10, 'A tour name must have at least 10 characters']
-      //validate: [validator.isAlpha, 'Tour name must only contain characters'] // validator.js validation
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters']
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -22,21 +21,22 @@ const tourSchema = new mongoose.Schema(
     },
     maxGroupSize: {
       type: Number,
-      required: [true, 'A tour must have a grpoup size']
+      required: [true, 'A tour must have a group size']
     },
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
       enum: {
-        values: ['easy', 'medium', 'difficult'], // Enum is a backend data validator for strings
-        message: 'Difficulty must be either easy, medium, difficult'
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
       }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must not be above 5.0']
+      max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
@@ -49,18 +49,17 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       validate: {
-        // custom data validator that we wrote ourself
         validator: function(val) {
-          // this only points to curent doc on NEW document creation
+          // this only points to current doc on NEW document creation
           return val < this.price;
         },
-        message: 'Discount price ({VALUE}) cannot be greater than price!' //({allows access to the value itself})
+        message: 'Discount price ({VALUE}) should be below regular price'
       }
     },
     summary: {
       type: String,
       trim: true,
-      required: ['A tour must have a descvription']
+      required: [true, 'A tour must have a description']
     },
     description: {
       type: String,
@@ -117,29 +116,47 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
-// GeoJson support and location data added to tourModel
 
-// Virtual properties wont be saved into database
-// Using a regular function instead of arrow functions as arrows dont get its own this keyword
-// virtual properties can't be queried in a url
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
-//Virtual populate
+// Virtual populate
 tourSchema.virtual('reviews', {
   ref: 'Review',
   foreignField: 'tour',
   localField: '_id'
 });
 
-// Document middleware: runs before .save() and .create()
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-// QUERY MIDDLEWARE that runs before find method to exclude secret tour from results
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
 
@@ -147,30 +164,28 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  //console.log(docs);
-  next();
-});
-
-// Populate the tour with guides
 tourSchema.pre(/^find/, function(next) {
   this.populate({
-    //poulate function is fundamental when working with mongoose
     path: 'guides',
     select: '-__v -passwordChangedAt'
   });
+
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-//datamodel https://mongoosejs.com/docs/models.html
+//   console.log(this.pipeline());
+//   next();
+// });
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
--- models/userModel.js
+++ models/userModel.js
@@ -6,24 +6,24 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'User must have a name']
+    required: [true, 'Please tell us your name!']
   },
   email: {
     type: String,
-    required: [true, 'Email is mandatory'],
+    required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'please provide a valid e-mail']
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
+  photo: String,
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
     default: 'user'
   },
-  photo: String,
   password: {
     type: String,
-    required: [true, 'User must enter a password'],
+    required: [true, 'Please provide a password'],
     minlength: 8,
     select: false
   },
@@ -31,11 +31,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
-      // This only works on create and save!!!
+      // This only works on CREATE and SAVE!!!
       validator: function(el) {
         return el === this.password;
       },
-      message: 'Passwords do not match!'
+      message: 'Passwords are not the same!'
     }
   },
   passwordChangedAt: Date,
@@ -49,21 +49,19 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function(next) {
-  // only run this function if password was modified
+  // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
 
-  // password is being encrypted with a bcrypt hash function, with cost of 12
+  // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
 
-  // delete the passwordConfirm field
+  // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
-// Added boolean type account information to userSchemma
-// Added a pre save middleware to return only users that dont have false
-// Update changedPasswordAt property for the user in DB
+
 userSchema.pre('save', function(next) {
-  if (!this.isModified('password' || this.isNew)) return next();
+  if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
   next();
@@ -71,7 +69,7 @@ userSchema.pre('save', function(next) {
 
 userSchema.pre(/^find/, function(next) {
   // this points to the current query
-  this.find({ active: { $ne: false } }); // returning all users that are not false, only the active ones
+  this.find({ active: { $ne: false } });
   next();
 });
 
@@ -79,10 +77,9 @@ userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
 ) {
-  return await bcrypt.compare(candidatePassword, userPassword); //compares if both passwords are same, and returns true if are
+  return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-//instance method
 userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
