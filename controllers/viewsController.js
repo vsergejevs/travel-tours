a5c74f25843f2193aee9a8e4ec1e2624878355e4
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-05-26 06:00:42 +0100

    getting the tour data for tour model

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data
+  const tour = await (await Tour.findOne({ slug: req.params.slug })).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
