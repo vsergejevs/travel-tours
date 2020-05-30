d08a8e53f7d003826dfaf8373db64a6f2f35ed0e
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-05-30 12:30:31 +0100

    added comments for reference

--- models/tourModel.js
+++ models/tourModel.js
@@ -36,7 +36,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
-      set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
+      set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7 - rounds ratings rreview to 4.7
     },
     ratingsQuantity: {
       type: Number,
@@ -118,6 +118,8 @@ const tourSchema = new mongoose.Schema(
 );
 
 // tourSchema.index({ price: 1 });
+//1 - ascending
+//-1 - descending
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
 tourSchema.index({ startLocation: '2dsphere' });
