5798755001e05c744ffb985272c9701d8ad3bf2f
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-06-01 05:22:51 +0100

    imported features from mapbox and login file

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,27 @@
-console.log('hello from parcel');
+/* eslint-disable */
+
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login } from './login';
+
+//console.log('hello from the client side');
+
+// DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+// DELEGATION
+
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+//console.log(locations);
+if (loginForm)
+  document.querySelector('.form').addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
