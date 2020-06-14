7f5b7972bc3be25bf511ae607c8b3e28ac09881d
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-06-14 07:33:19 +0100

    testing sending emails with sengrid

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,26 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
+//
 
-// creating a class
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Valeriy Sergeyev <${process.env.EMAIL_FROM}>`;
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return 1;
-      // return nodemailer.createTransport({
-      //   service: 'SendGrid',
-      //   auth: {
-      //     user: process.env.SENDGRID_USERNAME,
-      //     pass: process.env.SENDGRID_PASSWORD,
-      //   },
-      // });
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     }
 
     return nodemailer.createTransport({
@@ -34,6 +33,7 @@ module.exports = class Email {
     });
   }
 
+  // Send the actual email
   async send(template, subject) {
     // 1) Render HTML based on a pug template
     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
@@ -42,20 +42,27 @@ module.exports = class Email {
       subject
     });
 
-    // 2) Define the email options
+    // 2) Define email options
     const mailOptions = {
       from: this.from,
       to: this.to,
       subject,
       html,
       text: htmlToText.fromString(html)
-      // html:
     };
+
     // 3) Create a transport and send email
     await this.newTransport().sendMail(mailOptions);
   }
 
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
   }
 };
