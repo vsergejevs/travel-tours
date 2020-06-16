2be3b7fb0dbdd46492bbf68cccfe3486a6735071
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-06-16 05:40:38 +0100

    added event listener to booking button to book a tour

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 
@@ -60,4 +61,11 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
