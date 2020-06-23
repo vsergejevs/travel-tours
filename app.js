ee0a5c97ede86fb7e40d4f338fa24e2371be7777
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-06-23 04:55:15 +0100

    compression package added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -72,6 +73,8 @@ app.use((req, res, next) => {
   //console.log('hello from middleware');
   next();
 });
+
+app.use(compression());
 
 // Test middleware. Middleware to log request time in controllers
 app.use((req, res, next) => {
