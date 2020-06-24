3eb38587d0700c4dd3e13a42040ef1898adecde6
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-06-24 16:40:06 +0100

    updated port varoiable

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
 
 // console.log(process.env);
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
