d613bda727576b774c9eab69799a595602ff0ae5
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-07-13 04:23:38 +0100

    fix reviews on tour page

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,7 +1,6 @@
 // review / rating / createdAt / ref to tour / ref to user
 const mongoose = require('mongoose');
 const Tour = require('./tourModel');
-// const User = require('./userModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -39,18 +38,18 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function(next) {
-  this.populate({
-    path: 'tour',
-    select: 'name'
-  }).populate({
-    path: 'user',
-    select: 'name'
-  });
-
   // this.populate({
+  //   path: 'tour',
+  //   select: 'name'
+  // }).populate({
   //   path: 'user',
-  //   select: 'name photo'
+  //   select: 'name'
   // });
+
+  this.populate({
+    path: 'user',
+    select: 'name photo'
+  });
   next();
 });
 
